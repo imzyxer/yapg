e878fe572d73d174077d6c8e0ebfd19681a9c2fd
Anton Yanchuk <anton@yanchuk.name>
2022-05-22 16:55:12 +0300

    Add basic tests

--- src/Generator.ts
+++ src/Generator.ts
@@ -1,10 +1,10 @@
 export type TGeneratorOptions = {
-    numbers: boolean;
-    uppercase: boolean;
-    lowercase: boolean;
-    symbols: boolean,
-    length: number;
-    group: number;
+  numbers: boolean;
+  uppercase: boolean;
+  lowercase: boolean;
+  symbols: boolean,
+  length: number;
+  group: number;
 };
 
 type TRange = [number, number];
@@ -12,51 +12,73 @@ type TRanges = TRange[];
 type TRangeCollection = TRanges[];
 
 export class Generator {
-    protected options: TGeneratorOptions = {
-        numbers: true,
-        uppercase: true,
-        lowercase: true,
-        symbols: false,
-        length: 12,
-        group: 4,
-    };
+  protected options: TGeneratorOptions = {
+    numbers: true,
+    uppercase: true,
+    lowercase: true,
+    symbols: false,
+    length: 12,
+    group: 4,
+  };
 
-    public constructor(options?: Partial<TGeneratorOptions>) {
-        this.options = Object.assign({}, this.options, options);
+  public constructor(options?: Partial<TGeneratorOptions>) {
+    this.options = Object.assign({}, this.options, options);
+  }
+
+  public generate() {
+    const rangeCollections = this.getRangeCollection();
+    const charMap = this.getCharMap(rangeCollections);
+    const password = [];
+    while (password.length < this.options.length) {
+      const char = Math.random() * charMap.length | 0;
+      password.push(charMap[char]);
     }
+    if (!this.options.group) return password.join('');
 
-    public generate() {
-        const rangeCollections = this.getRangeCollection();
-        const charMap = this.getCharMap(rangeCollections);
-        const password = [];
-        while (password.length < this.options.length) {
-            const char = Math.random() * charMap.length | 0;
-            password.push(charMap[char]);
-        }
-        if (!this.options.group) return password.join('');
+    const re = new RegExp(`(.{1,${this.options.group}})`, 'g');
+    return password.join('').replace(re, '$1-').slice(0, -1);
+  }
 
-        const re = new RegExp(`(.{1,${this.options.group}})`, 'g');
-        return password.join('').replace(re, '$1-').slice(0, -1);
-    }
+  protected getRangeCollection(): TRangeCollection {
+    const collection = [];
+    if (this.options.numbers) collection.push(this.numbersRanges);
+    if (this.options.uppercase) collection.push(this.uppercaseRanges);
+    if (this.options.lowercase) collection.push(this.lowercaseRanges);
+    if (this.options.symbols) collection.push(this.symbolsRanges);
+    return collection;
+  }
 
-    protected getRangeCollection(): TRangeCollection {
-        const collection = [];
-        if (this.options.numbers) collection.push([[48, 57]] as TRanges);
-        if (this.options.uppercase) collection.push([[65, 90]] as TRanges);
-        if (this.options.lowercase) collection.push([[97, 122]] as TRanges);
-        if (this.options.symbols) collection.push([[33, 46], [58, 64], [94, 96], [123, 126]] as TRanges);
-        return collection;
-    }
+  private get numbersRanges(): TRanges {
+    return [[48, 57]];
+  }
 
-    protected getCharMap(rangeCollection: TRangeCollection) {
-        const map: string[] = [];
-        rangeCollection.forEach(ranges => {
-            ranges.forEach(range => {
-                for (let i = range[0]; i <= range[1]; i++) {
-                    map.push(String.fromCharCode(i));
-                }
-            })
-        })
-        return map;
-    }
+  private get uppercaseRanges(): TRanges {
+    return [[65, 90]];
+  }
+
+  private get lowercaseRanges(): TRanges {
+    return [[97, 122]];
+  }
+
+  private get symbolsRanges(): TRanges {
+    return [[33, 44], [58, 64], [94, 96], [123, 126]];
+  }
+
+  protected getCharMap(rangeCollection: TRangeCollection) {
+    let map: string[] = [];
+    rangeCollection.forEach(ranges => {
+      map = map.concat(this.getCharsByRanges(ranges));
+    });
+    return map;
+  }
+
+  protected getCharsByRanges(ranges: TRanges) {
+    const map: string[] = [];
+    ranges.forEach(range => {
+      for (let i = range[0]; i <= range[1]; i++) {
+        map.push(String.fromCharCode(i));
+      }
+    });
+    return map;
+  }
 }
--- test/Generator.test.ts
+++ test/Generator.test.ts
@@ -1,8 +1,43 @@
 import Generator from '../src';
 
-describe("Testing yet another password generator", () => {
-    test("pwassword 123", () => {
-        const passgen = new Generator();
-        expect(passgen.generate()).toEqual('123');
+const pgProto = Object.getPrototypeOf(new Generator());
+const numbers = pgProto.getCharsByRanges(pgProto.numbersRanges).join('');
+const reNumbers = new RegExp(`[${numbers}]+`);
+const uppercase = pgProto.getCharsByRanges(pgProto.uppercaseRanges).join('');
+const reUppercase = new RegExp(`[${uppercase}]+`);
+const lowercase = pgProto.getCharsByRanges(pgProto.lowercaseRanges).join('');
+const reLowercase = new RegExp(`[${lowercase}]+`);
+const symbols = pgProto.getCharsByRanges(pgProto.symbolsRanges).join('');
+const reSymbols = new RegExp(`[${symbols}]+`);
+
+describe("Yet another password generator test", () => {
+  describe("Generate password by default", () => {
+    const pg = new Generator();
+    const password = pg.generate();
+
+    it('Default length', () => expect(password.length).toBe(12 + 2));
+    it('Contains numbers', () => expect(password).toMatch(reNumbers));
+    it('Contains uppercase', () => expect(password).toMatch(reUppercase));
+    it('Contains lowercase', () => expect(password).toMatch(reLowercase));
+    it('Does not contains symbols', () => expect(password).not.toMatch(reSymbols));
+    it('Contains 3 groups', () => expect(password.split('-').length).toBe(3));
+  });
+  describe("Generate password by usual options", () => {
+    const pg = new Generator({
+      numbers: true,
+      uppercase: true,
+      lowercase: true,
+      symbols: true,
+      length: 10,
+      group: 0,
     });
+    const password = pg.generate();
+
+    it('Length have custom value', () => expect(password.length).toBe(10));
+    it('Contains numbers', () => expect(password).toMatch(reNumbers));
+    it('Contains uppercase', () => expect(password).toMatch(reUppercase));
+    it('Contains lowercase', () => expect(password).toMatch(reLowercase));
+    it('Contains symbols', () => expect(password).toMatch(reSymbols));
+    it('Does not contains groups', () => expect(password.split('-').length).toBe(1));
+  });
 });
